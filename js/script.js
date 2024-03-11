8fb046f5428ec341ad3b902a5ac606ac6a293423
simoneaucello <simoneaucello@gmail.com>
2024-03-11 16:19:18 +0100

    [add] clock

--- js/script.js
+++ js/script.js
@@ -5,7 +5,35 @@ createApp({
   data(){
     return {
       message: 'Hello Vue!',
-      image: 'Vue.png'
+      image: 'Vue.png',
+      displayClock: ''
     }
+  },
+
+  methods: {
+    myClock(){
+      const d = new Date();
+      const h = d.getHours() < 10 ? '0' + d.getHours() : d.getHours();
+      const m = d.getMinutes() < 10 ? '0' + d.getMinutes() : d.getMinutes();
+      const s = d.getSeconds() < 10 ? '0' + d.getSeconds() : d.getSeconds();
+      this.displayClock = `${h}:${m}:${s}`
+    },
+
+    startClock(){
+      setInterval(() => {
+        this.myClock()
+      },1000)
+
+    }
+
+  },
+
+  mounted(){
+    this.myClock();
+    this.startClock();
   }
+
+
+
+
 }).mount('#app')
